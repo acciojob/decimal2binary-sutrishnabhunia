0583ae55ef5853a367a7cfbbc0dc237202437188
Sutrishna Bhunia <134145442+sutrishnabhunia@users.noreply.github.com>
2023-07-20 14:43:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-function decimalToBinary(num) {
-  //Write you code here
-  
+function decimalToBinary(decimal) {
+  return decimal.toString(2);
 }
-
-window.decimalToBinary = decimalToBinary;
+ 
+console.log(decimalToBinary(7));
+console.log(decimalToBinary(10)); 
+console.log(decimalToBinary(33)); 
